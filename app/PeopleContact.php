7096943709503b74743ce138498e67e9f04b3d91
Andreas Beasley <andreas@sapioweb.com>
2016-01-29 08:08:50 -0800

    new people contact table model

---
+++ app/PeopleContact.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PeopleContact extends Model
+{
+	protected $fillable = [
+		'first_name',
+		'middle_name',
+		'last_name',
+		'birthday_day',
+		'birthday_month',
+		'birthday_year',
+		'gender',
+		'address_street',
+		'address_city',
+		'address_state',
+		'address_zip',
+		'country',
+		'home_phone',
+		'business_phone',
+		'mobile_phone',
+		'other_phone',
+		'fax',
+		'email_1',
+		'email_2',
+		'email_3',
+		'avatar',
+		'tax_id',
+		'notes',
+	];
+
+	public function companies()
+	{
+		return $this->belongsToMany('App\CompanyContact');
+	}
+}
