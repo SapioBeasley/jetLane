8bd3cac6cbe7109d74f0917b9299563d9163a6c1
Andreas Beasley <andreas@sapioweb.com>
2016-02-12 16:10:32 -0800

    bump travis

--- config/database.php
+++ config/database.php
@@ -49,7 +49,7 @@ return [
         'testing' => [
             'driver'    => 'mysql',
             'host'      => env('DB_TEST_HOST', 'localhost'),
-            'database'  => env('DB_TEST_DATABASE', 'homestead_test'),
+            'database'  => env('DB_TEST_DATABASE', 'test_db'),
             'username'  => env('DB_TEST_USERNAME', 'homestead'),
             'password'  => env('DB_TEST_PASSWORD', 'secret'),
             'charset'   => 'utf8',
