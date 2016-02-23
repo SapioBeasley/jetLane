57cbabb4a9f7a0576d552285bc0f1e5215dd56c0
Andreas Beasley <andreas@sapioweb.com>
2016-02-22 16:30:53 -0800

    loan fillable filled

--- app/Loan.php
+++ app/Loan.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Loan extends Model
 {
-    //
+	protected $fillable = [
+		'loan_title',
+		'loan_subtitle',
+		'fha',
+		'VA',
+		'Conventional',
+		'term',
+		'rate',
+		'min_down_payment',
+		'front_end_mortgate_insurence',
+		'bankrupcy_burnoff',
+		'foreclosure_burnoff',
+		'short_sale_burnoff',
+		'owner_occupant_only',
+		'second_home',
+		'two_loans',
+		'conforming_loan',
+		'lender_fee',
+		'origination_fee',
+		'front_end_limit',
+		'back_end_limit',
+		'contribution_limit',
+		'loan_limit',
+		'min_fico',
+	];
 }
