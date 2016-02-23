710eff8cc230be5d80b6b5a69deab6e8ddf8bde6
Andreas Beasley <andreas@sapioweb.com>
2016-02-22 16:31:19 -0800

    crudHelper added to app config

--- config/app.php
+++ config/app.php
@@ -157,6 +157,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
         Spatie\Tail\TailServiceProvider::class,
+        Sapioweb\CrudHelper\CrudHelperServiceProvider::class,
 
     ],
 
@@ -205,6 +206,7 @@ return [
         'View'      => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'CrudHelper' => Sapioweb\CrudHelper\CrudyController::class,
 
     ],
 
